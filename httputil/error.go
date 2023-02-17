aa8e7355de4a509fe2dd211e577416ab590bb0af
ayrton-hbtn <3282@holbertonschool.com>
2023-02-16 22:18:01 -0300

    .

--- httputil/error.go
+++ httputil/error.go
@@ -5,14 +5,14 @@ import "github.com/gin-gonic/gin"
 // NewError example
 func NewError(ctx *gin.Context, status int, err error) {
 	er := HTTPError{
-		Code:    status,
-		Message: err.Error(),
+		Code:  status,
+		Error: err.Error(),
 	}
 	ctx.JSON(status, er)
 }
 
 // HTTPError example
 type HTTPError struct {
-	Code    int    `json:"code" example:"400"`
-	Message string `json:"message" example:"status bad request"`
+	Code  int    `json:"code" example:"400"`
+	Error string `json:"error" example:"Invalid request payload"`
 }
